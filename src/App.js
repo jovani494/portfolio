8889d4c5daf54ce9437039318857d5f4fc54b463
Jovani Matthieu <ravelonarivojovani@gmail.com>
2024-03-18 22:09:08 +0300

    Component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Component } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        
+        <NavBar>
+
+        </NavBar>
+
+        <Footer />
       </div>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+class NavBar extends Component{
+    render(){
+        return(
+            <div className="navbar">
+
+            </div>
+        )
+    }
+}
+
+export default NavBar
